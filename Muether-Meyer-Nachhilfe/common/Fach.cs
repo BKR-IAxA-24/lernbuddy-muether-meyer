fe76ddabc9c8f3a676f800c98a2a77bd0dd4194a
LukasMeyer42 <1059146@bkrnet.de>
2025-03-19 13:26:48 +0100

    Neue Klasse Fach mit Eigenschaften und Konstruktoren
    Es wurde eine neue Klasse `Fach` in `Fach.cs` hinzugefügt.
    Die Klasse enthält private Felder für `fachID` und `bezeichnung`,
    sowie öffentliche Eigenschaften für den Zugriff. Zwei Konstruktoren
    wurden implementiert: ein parameterloser und einer mit Parametern
    für `fachID` und `bezeichnung`.

---
+++ Muether-Meyer-Nachhilfe/common/Fach.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Muether_Meyer_Nachhilfe.common
+{
+    internal class Fach
+    {
+
+		private int fachID;
+
+		private string bezeichnung;
+
+		public string Bezeichnung
+		{
+			get { return bezeichnung; }
+			set { bezeichnung = value; }
+		}
+
+
+
+		public int FachID
+		{
+			get { return fachID; }
+			set { fachID = value; }
+		}
+
+        public Fach()
+        {
+        }
+
+        public Fach(int fachID, string bezeichnung)
+        {
+            this.fachID = fachID;
+            this.bezeichnung = bezeichnung;
+        }
+
+
+
+
+
+        }
+}
