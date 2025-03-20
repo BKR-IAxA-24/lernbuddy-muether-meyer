9875b4698389097ab04d02ae2e171530df49258d
LukasMeyer42 <1059146@bkrnet.de>
2025-03-20 08:15:22 +0100

    Neue Klassen für Zeitspanne und Wochentag hinzugefügt
    In der Datei `Zeitspanne.cs` wurde die Klasse `Zeitspanne` implementiert, die Eigenschaften für `WochentagID`, `SchuelerID`, `Startzeit` und `Endzeit` enthält. Getter und Setter wurden hinzugefügt, und es gibt Validierungen für die Zeitwerte. Zudem wurde ein Konstruktor und eine Methode zur Formatierung der Zeit im HH:MM-Format erstellt.
    
    In der Datei `wochentag.cs` wurde die Klasse `wochentag` hinzugefügt, die `WochentagID` und `Tag` als Eigenschaften hat. Auch hier wurden Getter, Setter und ein Konstruktor zur Initialisierung der Eigenschaften implementiert.

---
+++ Muether-Meyer-Nachhilfe/common/Zeitspanne.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Muether_Meyer_Nachhilfe.common
+{
+    internal class Zeitspanne
+    {
+
+        private int wochentagID;
+        private int schuelerID;
+        
+        //FORMAT HHMM
+        private int startzeit;
+
+        //FORMAT HHMM
+        private int endzeit;
+
+        public int WochentagID
+        {
+            get { return wochentagID; }
+            set { wochentagID = value; }
+        }
+
+        public int SchuelerID
+        {
+            get { return schuelerID; }
+            set { schuelerID = value; }
+        }
+
+        public int Startzeit
+        {
+            get { return startzeit; }
+            set {
+
+                if (value < 0000 || value > 2400)
+                {
+                    startzeit = 0000;
+                }
+
+                startzeit = value; 
+            }
+        }
+
+        public int Endzeit
+        {
+            get { return endzeit; }
+            set
+            {
+
+                if (value < 0000 || value > 2400)
+                {
+                    endzeit = 0000;
+                }
+
+                endzeit = value;
+            }
+        }
+
+
+        public Zeitspanne()
+        {
+        }
+
+
+
+        public Zeitspanne(int wochentagID, int schuelerID, int startzeit, int endzeit)
+        {
+            WochentagID = wochentagID;
+            SchuelerID = schuelerID;
+            Startzeit = startzeit;
+            Endzeit = endzeit;
+        }
+
+        public string FormatTime(int time)
+        {
+            string timeString = time.ToString();
+            while (timeString.Length < 4)
+            {
+                timeString = "0" + timeString;
+            }
+            return $"{timeString.Substring(0, 2)}:{timeString.Substring(2, 2)}";
+        }
+
+    }
+}
---
+++ Muether-Meyer-Nachhilfe/common/wochentag.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Muether_Meyer_Nachhilfe.common
+{
+    internal class wochentag
+    {
+
+        private int wochentagID;
+        private string tag;
+
+
+        public string Tag
+        {
+            get { return tag; }
+            set { tag = value; }
+        }
+
+        public int WochentagID
+        {
+            get { return wochentagID; }
+            set { wochentagID = value; }
+        }
+
+
+        public wochentag()
+        {
+        }
+
+        public wochentag(int wochentagID, string tag)
+        {
+            this.wochentagID = wochentagID;
+            this.tag = tag;
+        }
+
+
+    }
+}
