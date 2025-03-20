e67784aace63995dc32fd7fd398c090e169f58b7
LukasMeyer42 <1059146@bkrnet.de>
2025-03-20 07:57:40 +0100

    Neue Klasse `lehrerhatfach` hinzugefügt
    Implementiert die Klasse `lehrerhatfach` im Namespace `Muether_Meyer_Nachhilfe.common` mit den privaten Feldern `tutorID` und `fachID`. Enthält öffentliche Eigenschaften `TutorID` und `FachID` sowie zwei Konstruktoren zur Initialisierung der Felder.

---
+++ Muether-Meyer-Nachhilfe/common/lehrerhatfach.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Muether_Meyer_Nachhilfe.common
+{
+    internal class lehrerhatfach
+    {
+
+        private int tutorID;
+        public int TutorID
+        {
+            get { return tutorID; }
+            set { tutorID = value; }
+        }
+        private int fachID;
+        public int FachID
+        {
+            get { return fachID; }
+            set { fachID = value; }
+        }
+        public lehrerhatfach()
+        {
+        }
+        public lehrerhatfach(int tutorID, int fachID)
+        {
+            this.tutorID = tutorID;
+            this.fachID = fachID;
+        }
+
+
+    }
+}
