6b230cc847905aa94f71e05238d80e110161e922
LukasMeyer42 <1059146@bkrnet.de>
2025-03-20 07:41:17 +0100

    Füge Klasse Tutor mit Eigenschaften und Methoden hinzu
    In der Datei `Tutor.cs` wurden Namespaces hinzugefügt und eine neue Klasse `Tutor` implementiert. Die Klasse enthält private Felder für `tutorID`, `schuelerID`, `genehmigt` und `loginID`, sowie öffentliche Eigenschaften für den Zugriff auf diese Felder. Die `Genehmigt`-Eigenschaft gibt einen booleschen Wert zurück, basierend auf `genehmigt`, und die Methode `getGenehmigtFromBool` wandelt einen booleschen Wert in einen float-Wert um. Diese Änderungen verbessern die Struktur und Funktionalität der Klasse.

---
+++ Muether-Meyer-Nachhilfe/common/Tutor.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Muether_Meyer_Nachhilfe.common
+{
+	internal class Tutor
+	{
+		private int tutorID;
+
+		public int TutorID
+		{
+			get { return tutorID; }
+			set { tutorID = value; }
+		}
+
+		private int schuelerID;
+
+		public int SchuelerID
+		{
+			get { return schuelerID; }
+			set { schuelerID = value; }
+		}
+
+
+		private float genehmigt;
+
+		public bool Genehmigt
+		{
+			get
+			{
+				if (genehmigt == 1)
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+			set
+			{
+
+				if (value == true)
+				{
+					genehmigt = 1;
+				}
+				else
+				{
+					genehmigt = 0;
+				}
+
+
+			}
+		}
+
+		public float getGenehmigtFromBool(bool genehmigt)
+		{
+
+            if (genehmigt == true)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+
+		private int loginID;
+
+		public int LoginID { get => loginID; set => loginID = value; }
+
+
+    }
+}
