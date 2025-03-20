7e8d24a8c2be62cd99cf119d6b4013acb03eafe7
LukasMeyer42 <1059146@bkrnet.de>
2025-03-20 08:04:22 +0100

    Neue Klasse 'Klasse' im Namespace hinzugefügt
    In der Datei Klasse.cs wurde die Klasse `Klasse` im Namespace `Muether_Meyer_Nachhilfe.common` implementiert. Sie enthält private Felder für `klassenID`, `bezeichnung` und `bildungsgangID`, sowie öffentliche Eigenschaften für den Zugriff auf diese Felder. Zusätzlich wurden zwei Konstruktoren erstellt: ein parameterloser und ein Konstruktor zur Initialisierung aller drei Felder.

---
+++ Muether-Meyer-Nachhilfe/common/Klasse.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Muether_Meyer_Nachhilfe.common
+{
+    internal class Klasse
+    {
+
+        private int klassenID;
+        private string bezeichnung;
+        private int bildungsgangID;
+
+        public int KlassenID
+        {
+            get { return klassenID; }
+            set { klassenID = value; }
+        }
+
+        public string Bezeichnung
+        {
+            get { return bezeichnung; }
+            set { bezeichnung = value; }
+        }
+
+        public int BildungsgangID
+        {
+            get { return bildungsgangID; }
+            set { bildungsgangID = value; }
+        }
+
+        public Klasse()
+        {
+        }
+
+        public Klasse(int klassenID, string bezeichnung, int bildungsgangID)
+        {
+            this.klassenID = klassenID;
+            this.bezeichnung = bezeichnung;
+            this.bildungsgangID = bildungsgangID;
+        }
+
+
+
+
+    }
+}
